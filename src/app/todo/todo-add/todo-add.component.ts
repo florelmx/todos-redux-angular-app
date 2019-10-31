61d38229c52669bcb58962ceb261032854010ae3
Leonardo Flores <leomisaelflores@gmail.com>
2019-10-31 12:02:24 -0500

    primer commit

---
+++ src/app/todo/todo-add/todo-add.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as fromTodo from '../todo.actions'
+ 
+@Component({
+   selector: 'app-todo-add',
+   templateUrl: './todo-add.component.html',
+   styles: []
+})
+export class TodoAddComponent implements OnInit {
+
+   txtInput: FormControl; // definicion de la propiedad 
+   constructor(
+      private store: Store<AppState>
+   ) { }
+
+   ngOnInit() {
+      this.txtInput = new FormControl('', Validators.required)
+   }
+
+   agregarTodo() {
+      console.log(this.txtInput.value)
+      console.log(this.txtInput.valid)
+
+      if(this.txtInput.invalid){
+         return;
+      }
+
+      const accion = new fromTodo.AgregarTodoAction(this.txtInput.value);
+      this.store.dispatch(accion)
+
+      this.txtInput.setValue('')
+   }
+
+}
